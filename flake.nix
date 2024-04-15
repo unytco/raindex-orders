edd97360a3a934ae7d6d0bf9defba699e9d4bd82
highonhopium <jks.hardy@gmail.com>
2024-04-15 12:13:29 +0100

    wip

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake for development workflows.";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rainix.url = "github:rainprotocol/rainix";
+  };
+
+  outputs = {self, rainix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = rainix.pkgs.${system};
+      in rec {
+        packages = rainix.packages.${system};
+        devShells = rainix.devShells.${system};
+      }
+    );
+
+}
