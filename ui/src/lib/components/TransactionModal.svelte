53383dd2361ae310e8b70c40c4851554dfd50158
Jamie Harding <jharding142@icloud.com>
2024-04-18 20:27:38 +0200

    extract modal

---
+++ ui/src/lib/components/TransactionModal.svelte
@@ -0,0 +1,55 @@
+<script lang='ts'>
+    import { TransactionStatus } from '$lib/types.ts';
+    import { Button, Modal, Spinner } from 'flowbite-svelte';
+    import transactionStore from '$lib/stores/transactionStore';
+</script>
+
+<Modal
+	on:close={() => transactionStore.reset()}
+	open={$transactionStore.status !== TransactionStatus.IDLE}
+>
+	<div class="p-4">
+		<div class="flex flex-col items-center justify-center gap-2">
+			{#if $transactionStore.status === TransactionStatus.PENDING_WALLET}
+				<Spinner size="10" color="orange" />
+				{$transactionStore.status}
+			{/if}
+			{#if $transactionStore.status === TransactionStatus.PENDING_TX}
+				<Spinner size="10" color="green" />
+				{$transactionStore.status}
+				<a
+					class="font-blue-500 hover:underline"
+					href={`https://sepolia.etherscan.io/tx/${$transactionStore.hash}`}
+					target="_blank">View transaction on Etherscan</a
+				>
+			{/if}
+
+			{#if $transactionStore.status === TransactionStatus.SUCCESS}
+				<div
+					class="mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-green-100 dark:bg-green-900"
+				>
+					<h1 class="text-2xl">✅</h1>
+				</div>
+				{$transactionStore.status}
+				<a
+					class="font-blue-500 hover:underline"
+					href={`https://sepolia.etherscan.io/tx/${$transactionStore.hash}`}
+					target="_blank">View transaction on Etherscan</a
+				>
+				<Button on:click={() => transactionStore.reset()}>Close</Button>
+			{/if}
+
+			{#if $transactionStore.status === TransactionStatus.ERROR}
+				<div
+					class="mb-2 flex h-16 w-16 items-center justify-center rounded-full bg-green-100 dark:bg-green-900"
+				>
+					<h1 class="text-2xl">❌</h1>
+				</div>
+				<div class="flex flex-col">
+					{$transactionStore.error}
+				</div>
+				<Button  on:click={() => transactionStore.reset()}>Close</Button>
+			{/if}
+		</div>
+	</div>
+</Modal>
