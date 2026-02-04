bf9c36d997f39856558a2970ff37f4e614e10a8b
zo-el <joelulahanna@gmail.com>
2026-02-04 15:05:43 -0600

    host on website

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		allowedHosts: ['.unyt.dev', 'hot-bridge.unyt.dev'],
+		host: true
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
